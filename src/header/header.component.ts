d0cab5c90129f25cf5bc345f10f087dc937acc08
rmz21 <73691415+rmz21@users.noreply.github.com>
2021-01-26 20:25:51 +0800

    Add files via upload

---
+++ src/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Subscription } from 'rxjs';
+import { AuthService } from './../auth/auth.service';
+import { DataStorageService } from './../shared/data-storage.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) { }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(
+      user => {
+        this.isAuthenticated = !user ? false : true;
+      }
+    )
+  }
+
+  onLogout(){
+    this.authService.logout();
+  }
+
+  onSaveData(){
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData(){
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
+  }
+
+}
