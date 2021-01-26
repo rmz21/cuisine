3ae06c12b0edaccc1db01b53275d923b7198bd48
rmz21 <73691415+rmz21@users.noreply.github.com>
2021-01-26 20:23:38 +0800

    Add files via upload

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { map, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn:'root'})
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService,
+                private router: Router){}
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        router: RouterStateSnapshot
+    ): boolean | UrlTree | Promise<boolean> | Observable<boolean | UrlTree> {
+        return this.authService.user.pipe(
+            take(1),
+            map(user => {
+            const isAuth = !!user; // !user? false:true;
+            if(isAuth){
+                return true
+            } 
+            return this.router.createUrlTree(['/auth']);
+        }));
+    }
+}
